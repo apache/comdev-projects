3603910cb1af716d3af728a4bd5e33ec4fa9906d
Sebb <sebb@apache.org>
2018-07-10 15:01:52 +0000

    reporter now has TEST branch as well
    git-svn-id: https://svn.apache.org/repos/asf/comdev/projects.apache.org/branches/TEST_INSTALL@1835562 13f79535-47bb-0310-9956-ffa450edef68

--- scripts/cronjobs/pubsubber.sh
+++ scripts/cronjobs/pubsubber.sh
@@ -22,8 +22,8 @@ then
     python3 -u $SCRIPT stop
 else
     ERRTEE=1 LOGFILE=$LOGDIR/${BASE}_${YYMM}_daemon.log python3 -u $SCRIPT start \
-       comdev/projects.apache.org/branches/TEST_INSTALL /var/www/projects.apache.org/
-#       comdev/reporter.apache.org /var/www/reporter.apache.org/
+       comdev/projects.apache.org/branches/TEST_INSTALL /var/www/projects.apache.org/ \
+       comdev/reporter.apache.org/branches/TEST_INSTALL /var/www/reporter.apache.org/
     echo "Remember to start pubsubber again" >&2
 fi
 
