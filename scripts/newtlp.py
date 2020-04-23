e854761afb78de086470de7efaf9a13979e86e5b
Sebb <sebb@apache.org>
2020-04-23 10:40:32 +0000

    Docco
    git-svn-id: https://svn.apache.org/repos/asf/comdev/projects.apache.org/trunk@1876876 13f79535-47bb-0310-9956-ffa450edef68

--- scripts/newtlp.py
+++ scripts/newtlp.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
 """
+Creates the initial RDF for a new TLP
 
 Reads:
 data/committees/_template.rdf
-committee-info.txt from Whimsy
+committee-info.json from Whimsy
 
 Updates:
 data/committees.xml
