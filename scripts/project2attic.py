83a274f9307d8a9903553ef76d791c640abeb3fc
Sebb <sebb@apache.org>
2021-03-05 13:13:07 +0000

    Also retire PMC RDF file
    git-svn-id: https://svn.apache.org/repos/asf/comdev/projects.apache.org/trunk@1887199 13f79535-47bb-0310-9956-ffa450edef68

--- scripts/project2attic.py
+++ scripts/project2attic.py
@@ -4,6 +4,8 @@
 Updates a project to the attic:
 - creates an override DOAP under data/projects-override
 - updates projects.xml to point to the override DOAP
+- moves data/committees/<pmc>.rdf to data/committees-retired/<pmc>.rdf
+- updates data/committees.xml
 
 Does not commit the changes.
 
@@ -11,9 +13,15 @@ N.B. The current implementation does not handle PMCs with multiple projects.
 
 Updates:
 data/projects.xml
+data/committees.xml
 
 Creates:
 data/projects-override/<pmc>[_project].rdf
+
+Moves:
+data/committees/<pmc>.rdf
+to
+data/committees-retired/<pmc>.rdf
 
 """
 
@@ -22,6 +30,7 @@ from os.path import dirname, abspath, join
 from inspect import getsourcefile
 import os
 import re
+import subprocess
 
 if len(sys.argv) == 1:
     print("Please provide a list of project ids")
@@ -30,6 +39,43 @@ if len(sys.argv) == 1:
 OVERRIDE_DIR='https://svn.apache.org/repos/asf/comdev/projects.apache.org/trunk/data/projects-override'
 MYHOME = dirname(abspath(getsourcefile(lambda:0)))
 datadir = join(dirname(MYHOME),'data')
+
+def update_pmc_xml(pmc):
+    xmlfile = join(datadir,'committees.xml')
+    xmlfilet = join(datadir,'committees.xml.t')
+    print("Updating committees.xml")
+    found = 0
+    with open(xmlfile,'r') as r, open(xmlfilet,'w') as w:
+        for l in r:
+            m = re.search("^(\s+)<location>(.+)</location",l)
+            if m:
+                indent = m.group(1)
+                url = m.group(2)
+                # match one of:
+                # committees/<pmc>.rdf
+                # https://ofbiz.apache.org/<pmc>/...
+                # http://svn.apache.org/repos/asf/<pmc>/...
+                regex = "^(committees/%s\.rdf|https?://%s\.apache\.org/.+|https?://svn.apache.org/repos/asf/%s/.+)$" % (pmc,pmc,pmc)
+                if re.search(regex, url, flags=re.IGNORECASE):
+                    print("Found %s at %s" % (pmc, url))
+                    if url.startswith('committees/'):
+                        new = url.replace('committees/','committees-retired/')
+                        subprocess.run(["svn", "mv", "data/%s" % url, "data/%s" % new])
+                        url = new
+                    w.write("%s<!-- Retired: location>%s</location -->\n" % (indent, url))
+                    found += 1
+                    continue
+            w.write(l) # write the original line
+    if found == 1:
+        print("Found OK")
+        os.system("diff %s %s" % (xmlfile, xmlfilet))
+        os.rename(xmlfilet, xmlfile)
+    elif found > 1:
+        print("Matched more than once %d" % found)
+        os.remove(xmlfilet)
+    else:
+        print("Not found")
+        os.remove(xmlfilet)
 
 # update the DOAP to be an attic project
 def update_doap(doap, source):
@@ -51,7 +97,7 @@ def update_doap(doap, source):
     os.system("diff %s*" % (infile))
     os.rename(tmpfile,infile)
     
-def update_xml(pid):
+def update_project_xml(pid):
     xmlfile = join(datadir,'projects.xml')
     xmlfilet = join(datadir,'projects.xml.t')
     doapfile = join(datadir,'projects-override',"%s.rdf" % pid)
@@ -98,4 +144,5 @@ for arg in sys.argv[1:]:
     if not re.search("^[-a-z0-9]+$",arg):
         print("Expecting lower-case, '-' or digits only")
     else:
-        update_xml(arg)
+        update_pmc_xml(arg)
+        update_project_xml(arg)
