ff9dc3fffc6ee49579516f0bcefeb04906485cdc
Sebb <sebb@apache.org>
2023-09-06 12:39:35 +0000

    Ensure minimal fields are completed
    git-svn-id: https://svn.apache.org/repos/asf/comdev/projects.apache.org/trunk@1912135 13f79535-47bb-0310-9956-ffa450edef68

--- site/js/validate_form.js
+++ site/js/validate_form.js
@@ -3,6 +3,34 @@ function validate_form ( )
 {
     valid = true;
 
+    if ( document.make_doap.name.value == "" )
+    {
+        alert ( "Please fill in the project name field." );
+        document.make_doap.name.focus();
+        valid = false;
+    }
+
+    if ( document.make_doap.pmc.value == "" )
+    {
+        alert ( "Please fill in the PMC name field." );
+        document.make_doap.pmc.focus();
+        valid = false;
+    }
+
+    if ( document.make_doap.sdesc.value == "" )
+    {
+        alert ( "Please fill in the short description field." );
+        document.make_doap.sdesc.focus();
+        valid = false;
+    }
+
+    if ( document.make_doap.ldesc.value == "" )
+    {
+        alert ( "Please fill in the long description field." );
+        document.make_doap.ldesc.focus();
+        valid = false;
+    }
+
     if ( document.make_doap.std_title.value != "" && document.make_doap.std_id.value == "" )
     {
         alert ( "Please fill in the Implemented Standard 'ID' field." );
