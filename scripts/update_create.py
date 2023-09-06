f3c06861e8b0594626e3e9b0dec19a3356d405cd
Sebb <sebb@apache.org>
2023-09-06 15:20:49 +0000

    Add script to update create.html from JSON files
    git-svn-id: https://svn.apache.org/repos/asf/comdev/projects.apache.org/trunk@1912143 13f79535-47bb-0310-9956-ffa450edef68

---
+++ scripts/update_create.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+# @(#) update create.html from JSON files
+
+import os
+import json
+import re
+
+# This currently reads data at load time
+import committee_info
+SITEDIR = os.path.join(committee_info.COMDEV_HOME, 'site')
+
+validation = {}
+with open(os.path.join(SITEDIR, "validation.json")) as f:
+    validation = json.loads(f.read())
+lang = validation['languages']
+cats = validation['categories']
+
+createfile = os.path.join(SITEDIR, "create.html")
+createfilet = os.path.join(SITEDIR, "create.html.t")
+with open(createfile,'r') as r, open(createfilet,'w') as w:
+    section = None
+    line = 0
+    for l in r:
+        # start of a section?
+        m = re.match('^\s+<select name="(cat|lang|pmc)">', l)
+        if m:
+            section = m.group(1)
+            line = 0
+         # end of section, dump its data
+        if section and '</select>' in l:
+            if section == 'cat':
+                for k, v in cats.items():
+                    w.write(f'        <option value="{k}">{v}</option>\n')
+            elif section == 'lang':
+                for k, v in lang.items():
+                    w.write(f'        <option value="{k}">{v}</option>\n')
+            elif section == 'pmc':
+                lastcap = ''
+                for k,v in sorted(committee_info.pmcnames().items()):
+                    cap = k[0].upper()
+                    if cap != lastcap:
+                        w.write(f"\n      <!-- {cap} -->\n")
+                        lastcap = cap
+                    w.write(f'        <option value="{k}">{v}</option>\n')
+            section = None
+            line = 0
+        if section:
+            line = line + 1
+            if re.match('^\s*<option value="\w', l): # an existing option line
+                continue # drop the line
+            if section == 'pmc':
+                if re.match("      <!-- [A-Z] -->", l) or (re.match("\s*$", l) and line > 2):
+                    continue
+        w.write(l) # write the original line
+os.rename(createfilet, createfile)
