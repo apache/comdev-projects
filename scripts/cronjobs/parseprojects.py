5a3adb19f2eab405852e05c826e39f3402a6eb32
Sebb <sebb@apache.org>
2023-09-10 14:18:48 +0000

    Cannot have \n in subject!
    git-svn-id: https://svn.apache.org/repos/asf/comdev/projects.apache.org/trunk@1912226 13f79535-47bb-0310-9956-ffa450edef68

--- scripts/cronjobs/parseprojects.py
+++ scripts/cronjobs/parseprojects.py
@@ -426,7 +426,8 @@ if len(failures) > 0:
         f.close()
         if unreportedError:
             s = "\n".join(failures)
-            printMail("ERROR: one or more errors detected - see also the parseprojects.py log file\nURLs:\n%s" % s)
+            printMail("ERROR: one or more errors detected - see also the parseprojects.py log file",
+                      body="URLs:\n%s" % s)
 else:
     if os.path.exists("parseprojects-failures.xml"):
         print("No failures detected, removing previous failure data")
