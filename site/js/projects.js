f669e117ce20c5fc05f229dce9bee5a1926e1e98
Sebb <sebb@apache.org>
2025-06-09 09:20:12 +0000

    Don't try to guess how many projects there are in total
    git-svn-id: https://svn.apache.org/repos/asf/comdev/projects.apache.org/trunk@1926275 13f79535-47bb-0310-9956-ffa450edef68

--- site/js/projects.js
+++ site/js/projects.js
@@ -1035,15 +1035,12 @@ function renderFrontPage() {
     var numprojectsWithDoaps = 0; // how many projects have registered DOAPs
     for (i in projectsWithDoaps) numprojectsWithDoaps++;
     numProjects += (numcommittees - numprojectsWithDoaps); // Add in projects without DOAPs
-    var initiatives = numProjects + curPodlings; // both PMC and podlings
-    initiatives -= initiatives % 50; // round down to nearest 50
     var obj = document.getElementById('details');
     if (urlErrors.length > 0) {
         obj.innerHTML += "<p><span style='color: red'><b>Warning: could not load: "+urlErrors.join(', ')+"</b></span></p>"
     }
     update_id('loading', ''); // clear the loading messages
     // N.B. These ids must agree with the 'details' div in index.html
-    update_id('initiatives', initiatives);
     update_id('committees', numcommittees);
     update_id('projects', numProjects);
     update_id('podlings', curPodlings);
