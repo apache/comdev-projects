4419d173edea08a6f399cd4ba601c76f3924cf50
Sebb <sebb@apache.org>
2025-06-18 10:06:27 +0000

    Don't create unnecessary global variables
    git-svn-id: https://svn.apache.org/repos/asf/comdev/projects.apache.org/trunk@1926539 13f79535-47bb-0310-9956-ffa450edef68

--- scripts/cronjobs/generaterepos.py
+++ scripts/cronjobs/generaterepos.py
@@ -33,33 +33,36 @@ class SVNRepoParser(HTMLParser):
             repos[committee + '-svn'] = 'https://svn.apache.org/repos/asf/' + committee + '/'
 
 
-# Parse svn repos
-try:
-    svnResponse = requests.get("https://svn.apache.org/repos/asf/", timeout=120)
-    svnResponse.raise_for_status()
+def main():
+    # Parse svn repos
+    try:
+        svnResponse = requests.get("https://svn.apache.org/repos/asf/", timeout=120)
+        svnResponse.raise_for_status()
 
-    parser = SVNRepoParser()
-    parser.feed(svnResponse.content.decode("utf-8"))
-except requests.exceptions.RequestException as e:  # This is the correct syntax
-    print("ERROR: Unable to retrieve svn repos: %s", e)
+        parser = SVNRepoParser()
+        parser.feed(svnResponse.content.decode("utf-8"))
+    except requests.exceptions.RequestException as e:  # This is the correct syntax
+        print("ERROR: Unable to retrieve svn repos: %s", e)
 
 
-# Parse git repos
-try:
-    gitResponse = requests.get("https://gitbox.apache.org/repositories.json", timeout=120)
-    gitResponse.raise_for_status()
-    gitData = json.loads(gitResponse.content.decode("utf-8"))
+    # Parse git repos
+    try:
+        gitResponse = requests.get("https://gitbox.apache.org/repositories.json", timeout=120)
+        gitResponse.raise_for_status()
+        gitData = json.loads(gitResponse.content.decode("utf-8"))
 
-    for committee in gitData['projects']:
-        for repo in gitData['projects'][committee]['repositories']:
-            repos[repo] = 'https://gitbox.apache.org/repos/asf/' + repo + '.git'
-except requests.exceptions.RequestException as e:  # This is the correct syntax
-    print("ERROR: Unable to retrieve git repos: %s", e)
+        for committee in gitData['projects']:
+            for repo in gitData['projects'][committee]['repositories']:
+                repos[repo] = 'https://gitbox.apache.org/repos/asf/' + repo + '.git'
+    except requests.exceptions.RequestException as e:  # This is the correct syntax
+        print("ERROR: Unable to retrieve git repos: %s", e)
 
-print("Writing json/foundation/repositories.json...")
-with open("../../site/json/foundation/repositories.json", "w", encoding='utf-8') as f:
-    json.dump(repos, f, sort_keys=True, indent=0)
-    f.close()
+    print("Writing json/foundation/repositories.json...")
+    with open("../../site/json/foundation/repositories.json", "w", encoding='utf-8') as f:
+        json.dump(repos, f, sort_keys=True, indent=0)
+        f.close()
 
-print("All done!")
+    print("All done!")
 
+if __name__ == '__main__':
+    main()
