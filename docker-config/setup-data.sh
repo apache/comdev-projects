bc0a0ba9fb8be4bb0763f9d6dd024e2cc4600f58
Sebb <sebb@apache.org>
2026-02-19 23:04:53 +0000

    Docco
    git-svn-id: https://svn.apache.org/repos/asf/comdev/projects.apache.org/trunk@1931942 13f79535-47bb-0310-9956-ffa450edef68

--- docker-config/setup-data.sh
+++ docker-config/setup-data.sh
@@ -5,6 +5,6 @@ cd scripts/cronjobs
 python3 podlings.py
 python3 parsecommitters.py
 python3 parsereleases.py
-python3 parsecommitteeinfo.py
-python3 parseprojects.py
+python3 parsecommitteeinfo.py # slow
+python3 parseprojects.py # slow
 python3 generaterepos.py
