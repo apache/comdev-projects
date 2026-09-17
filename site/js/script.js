d048a05803801c173864e5062a99573864ec3a7e
Rich Bowen <rbowen@apache.org>
2026-09-17 19:15:41 +0000

    Consolidate nav bar into shared nav.html loaded via JS
    All 16 HTML pages now use an empty <div id='cssmenu'> shell.
    script.js fetches /nav.html and injects it, highlighting the active
    page based on a URL-to-label map. One place to edit the nav going forward.
    
    git-svn-id: https://svn.apache.org/repos/asf/comdev/projects.apache.org/trunk@1938329 13f79535-47bb-0310-9956-ffa450edef68

--- site/js/script.js
+++ site/js/script.js
@@ -1,5 +1,38 @@
 ( function( $ ) {
 $( document ).ready(function() {
 $('#cssmenu').prepend('<div id="bg-one"></div><div id="bg-two"></div><div id="bg-three"></div><div id="bg-four"></div>');
+
+// Load shared nav
+$.get('/nav.html', function(html) {
+   $('#cssmenu').append(html);
+   // Highlight the active page
+   var page = location.pathname.split('/').pop() || 'index.html';
+   // Map pages to their nav label
+   var navMap = {
+      'index.html':             'Home',
+      '':                       'Home',
+      'committees.html':        'Committees',
+      'committee.html':         'Committees',
+      'projects.html':          'Projects',
+      'project.html':           'Projects',
+      'datatables.html':        'Projects',
+      'releases.html':          'Releases',
+      'timelines.html':         'Timelines',
+      'about.html':             'About',
+      'doap.html':              'About',
+      'doapfaq.html':           'About',
+      'create.html':            'About',
+      'guidelines.html':        'About',
+      'pmc_rdf.html':           'About'
+   };
+   var active = navMap[page];
+   if (active) {
+      $('#cssmenu ul li a span').each(function() {
+         if ($(this).text() === active) {
+            $(this).closest('li').addClass('active');
+         }
+      });
+   }
+});
 });
 } )( jQuery );
